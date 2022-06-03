1da787890c1b21608d68a2e442221f1a779572a1
Dharmesh <onlinedk2k@gmail.com>
2022-06-03 21:51:51 +0530

    first comm

---
+++ Memo-Frontend/src/Components/About.jsx
@@ -0,0 +1,8 @@
+import React, { useContext, useEffect } from "react";
+import NoteContext from "../Context/Notes/NoteContext";
+
+function About() {
+  return <div>This is </div>;
+}
+
+export default About;
